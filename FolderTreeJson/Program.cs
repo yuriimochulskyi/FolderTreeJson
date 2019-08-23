add5f3f9e49759f57c98db5e4e57c84d21e19edb
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-08-23 11:35:44 +0300

    create form view

--- FolderTreeJson/Program.cs
+++ FolderTreeJson/Program.cs
@@ -16,7 +16,7 @@ namespace FolderTreeJson
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FolderTreeJson());
         }
     }
 }
